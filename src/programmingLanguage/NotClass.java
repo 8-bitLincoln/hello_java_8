122e461dff83e56c243537e39ec3b6204fac1a18
Dmitry Tsylind <nazwa@mail.ru>
2018-06-04 16:50:22 +0200

    piu

--- src/programmingLanguage/NotClass.java
+++ src/programmingLanguage/NotClass.java
@@ -3,3 +3,4 @@ class NotFound { private String nothing; }
 class NotFound { private String something; }
 class NotFound { private String some; }
 class NotFound { private String some1n; }
+class NotFound { private String somebody; }
