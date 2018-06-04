2851c28dd81f1e6e7babf87499530613fa03bc51
Dmitry Tsylind <nazwa@mail.ru>
2018-06-04 16:42:48 +0200

    ok?

--- src/programmingLanguage/NotClass.java
+++ src/programmingLanguage/NotClass.java
@@ -2,3 +2,4 @@ class NotFound {}
 class NotFound { private String nothing; }
 class NotFound { private String something; }
 class NotFound { private String some; }
+class NotFound { private String some1n; }
