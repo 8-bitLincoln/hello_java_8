c2db4302db506d7ed131075cac3e6beb1f0b47ef
Dmitry Tsylind <nazwa@mail.ru>
2018-06-04 17:30:23 +0200

    commit2

--- src/programmingLanguage/NotClass.java
+++ src/programmingLanguage/NotClass.java
@@ -5,3 +5,4 @@ class NotFound { private String some; }
 class NotFound { private String some1n; }
 class NotFound { private String somebody; }
 class NotFound { private String something; }
+class NotFound { private String something123; }
